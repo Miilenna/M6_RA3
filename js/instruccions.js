9db588c91c5e4b599a581ac57bfeae31fd7d47bf
Miilenna <milena200.fv@gmail.com>
2024-11-14 22:45:51 +0100

    comentarios añadidos

--- js/instruccions.js
+++ js/instruccions.js
@@ -1,6 +1,10 @@
+//Variable
 let tancaFinestra = document.getElementById("tancaFinestra");
+
+//evento para cerrar la ventana
 tancaFinestra.addEventListener("click", tanca);
 
+//función para cerrar la ventana de las instrucciones
 function tanca() {
     window.close()
 }
